b71656ddaa5ceecc38fca54cfa7f33630e57ea03
kozlika <kozlika@localhost>
2011-05-03 08:51:19 +0200

    Formulaires pages de niveau 2 (attention, geek needed pour revoir les fillres antispan auxquels je n'ai pas touche?)

---
+++ plugins/pages/_admin.php
@@ -0,0 +1,37 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+#
+# This file is part of Dotclear 2.
+#
+# Copyright (c) 2003-2010 Olivier Meunier & Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------
+if (!defined('DC_CONTEXT_ADMIN')) { return; }
+
+$core->addBehavior('adminDashboardIcons','pages_dashboard');
+$core->addBehavior('adminDashboardFavs','pages_dashboard_favs');
+function pages_dashboard($core,$icons)
+{
+	$icons['pages'] = new ArrayObject(array(__('Pages'),'plugin.php?p=pages','index.php?pf=pages/icon-big.png'));
+}
+function pages_dashboard_favs($core,$favs)
+{
+	$favs['pages'] = new ArrayObject(array('pages',__('Pages'),'plugin.php?p=pages',
+		'index.php?pf=pages/icon.png','index.php?pf=pages/icon-big.png',
+		'contentadmin,pages',null,null));
+	$favs['newpage'] = new ArrayObject(array('newpage',__('New page'),'plugin.php?p=pages&amp;act=page',
+		'index.php?pf=pages/icon-np.png','index.php?pf=pages/icon-np-big.png',
+		'contentadmin,pages',null,null));
+}
+
+$_menu['Blog']->addItem(__('Pages'),'plugin.php?p=pages','index.php?pf=pages/icon.png',
+		preg_match('/plugin.php\?p=pages(&.*)?$/',$_SERVER['REQUEST_URI']),
+		$core->auth->check('contentadmin,pages',$core->blog->id));
+
+$core->auth->setPermissionType('pages',__('manage pages'));
+
+require dirname(__FILE__).'/_widgets.php';
+?>
