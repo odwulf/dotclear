ff1c8da7e7844a158372143db229b4e917ef39ab
Pascal Chevrel <pascal.chevrel@free.fr>
2013-08-13 15:05:35 +0200

    Ticket #1526 : antispam, extract https links from comments

---
+++ plugins/importExport/_define.php
@@ -0,0 +1,21 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+#
+# This file is part of importExport, a plugin for DotClear2.
+#
+# Copyright (c) 2003-2012 Olivier Meunier & Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */			"Import / Export",
+	/* Description*/		"Import and Export your blog",
+	/* Author */			"Olivier Meunier & Contributors",
+	/* Version */			'3.2',
+	/* Perm */			'admin'
+);
+?>
