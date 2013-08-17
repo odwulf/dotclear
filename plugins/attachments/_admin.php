9a7ceb9f7bddcbfeb652a93e466cac8d0fd0e502
Denis Jean-Christian <jcdenis@users.noreply.github.com>
2013-08-18 00:20:56 +0200

    Write blowUp CSS into a public file on update, like images, fixes #736

---
+++ plugins/attachments/_admin.php
@@ -0,0 +1,80 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+#
+# This file is part of Dotclear 2.
+#
+# Copyright (c) 2003-2013 Olivier Meunier & Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------
+if (!defined('DC_CONTEXT_ADMIN')) { return; }
+
+$core->addBehavior ('adminPostFormItems',array('attachmentAdmin','adminPostFormItems'));
+$core->addBehavior ('adminPostAfterForm',array('attachmentAdmin','adminPostAfterForm'));
+$core->addBehavior('adminPostHeaders',array('attachmentAdmin','postHeaders'));
+
+class attachmentAdmin
+{
+	public static function postHeaders()
+	{
+		return 
+		'<script type="text/javascript" src="index.php?pf=attachments/js/post.js"></script>';
+	}
+	public static function adminPostFormItems($main,$sidebar,$post) 
+	{
+		if ($post !== null)
+		{
+			$core =& $GLOBALS['core'];
+			$post_media = $core->media->getPostMedia($post->post_id);
+			$item = '<h5 class="clear s-attachments">'.__('Attachments').'</h5>';
+			foreach ($post_media as $f)
+			{
+				$ftitle = $f->media_title;
+				if (strlen($ftitle) > 18) {
+					$ftitle = substr($ftitle,0,16).'...';
+				}
+				$item .=
+				'<div class="media-item s-attachments">'.
+				'<a class="media-icon" href="media_item.php?id='.$f->media_id.'">'.
+				'<img src="'.$f->media_icon.'" alt="" title="'.$f->basename.'" /></a>'.
+				'<ul>'.
+				'<li><a class="media-link" href="media_item.php?id='.$f->media_id.'"'.
+				'title="'.$f->basename.'">'.$ftitle.'</a></li>'.
+				'<li>'.$f->media_dtstr.'</li>'.
+				'<li>'.files::size($f->size).' - '.
+				'<a href="'.$f->file_url.'">'.__('open').'</a>'.'</li>'.
+				
+				'<li class="media-action"><a class="attachment-remove" id="attachment-'.$f->media_id.'" '.
+				'href="post_media.php?post_id='.$post->post_id.'&amp;media_id='.$f->media_id.'&amp;remove=1">'.
+				'<img src="images/check-off.png" alt="'.__('remove').'" /></a>'.
+				'</li>'.
+				
+				'</ul>'.
+				'</div>';
+			}
+			unset($f);
+			
+			if (empty($post_media)) {
+				$item .= '<p class="form-note s-attachments">'.__('No attachment.').'</p>';
+			} 
+			$item .= '<p class="s-attachments"><a class="button" href="media.php?post_id='.$post->post_id.'">'.__('Add files to this entry').'</a></p>';
+			$sidebar['metas-box']['items']['attachments']= $item;
+		}
+	}
+	
+	public static function adminPostAfterForm($post) {
+		if ($post !== null)
+		{
+			$core =& $GLOBALS['core'];
+			echo
+				'<form action="post_media.php" id="attachment-remove-hide" method="post">'.
+				'<div>'.form::hidden(array('post_id'),$post->post_id).
+				form::hidden(array('media_id'),'').
+				form::hidden(array('remove'),1).
+				$core->formNonce().'</div></form>';
+		}
+	}
+}
+?>
