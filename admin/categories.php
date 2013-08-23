bce6b4df761158aa78f7aad2f2f38a21245e2040
franck <carnet.franck.paul@gmail.com>
2013-08-23 17:43:41 +0200

    Add dcPage::success function, addresses #1579

---
+++ admin/categories.php
@@ -0,0 +1,231 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+#
+# This file is part of Dotclear 2.
+#
+# Copyright (c) 2003-2013 Olivier Meunier & Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------
+
+require dirname(__FILE__).'/../inc/admin/prepend.php';
+
+dcPage::check('categories');
+
+# Remove a categories
+if (!empty($_POST['categories'])) {
+	try {
+		# Check if category where to move posts exists
+		$mov_cat = (int) $_POST['mov_cat'];
+		$mov_cat = $mov_cat ? $mov_cat : null;
+		if ($mov_cat !== null) {
+			$c = $core->blog->getCategory($mov_cat);
+			if ($c->isEmpty()) {
+				throw new Exception(__('Category where to move posts does not exist'));
+			}
+			unset($c);
+
+			if (in_array($mov_cat, $_POST['categories'])) {
+				throw new Exception(__('The entries cannot be moved to the category you choose to delete.'));
+			}
+		}
+
+		foreach ($_POST['categories'] as $cat_id) {
+			# Check if category to delete exists
+			$c = $core->blog->getCategory((integer) $cat_id);
+			if ($c->isEmpty()) {
+				continue;
+			}
+			unset($c);
+
+			# Move posts
+			if ($mov_cat != $cat_id) {
+			        $core->blog->changePostsCategory($cat_id,$mov_cat);
+			}
+
+			# Delete category
+			$core->blog->delCategory($cat_id);
+		}
+		http::redirect('categories.php?del='.count($_POST['categories']));
+	} catch (Exception $e) {
+		$core->error->add($e->getMessage());
+	}
+}
+
+# Update order
+if (!empty($_POST['categories_order']))
+{
+        $categories = json_decode($_POST['categories_order']);
+
+	foreach ($categories as $category) {
+	        if (!empty($category->item_id)) {
+		        $core->blog->updCategoryPosition($category->item_id, $category->left, $category->right);
+		}
+	}
+	http::redirect('categories.php?reord=1');
+}
+
+# Reset order
+if (!empty($_POST['reset']))
+{
+	try
+	{
+		$core->blog->resetCategoriesOrder();
+		http::redirect('categories.php?reord=1');
+	}
+	catch (Exception $e)
+	{
+		$core->error->add($e->getMessage());
+	}
+}
+
+# Actions combo box
+$combo_action = array();
+if ($core->auth->check('categories',$core->blog->id)) {
+	$combo_action[__('Delete')] = 'delete';
+}
+
+
+/* Display
+-------------------------------------------------------- */
+$rs = $core->blog->getCategories(array('post_type'=>'post'));
+
+$starting_script = dcPage::jsToolMan()."\n";
+if (!$core->auth->user_prefs->accessibility->nodragdrop
+	&& $core->auth->check('categories',$core->blog->id)
+	&& $rs->count()>1) {
+        $starting_script .= dcPage::jsLoad('js/jquery/jquery-ui.custom.js');
+	$starting_script .= dcPage::jsLoad('js/jquery/jquery.mjs.nestedSortable.js');
+}
+$starting_script .= dcPage::jsLoad('js/_categories.js');
+
+dcPage::open(__('Categories'),$starting_script,
+	dcPage::breadcrumb(
+		array(
+			html::escapeHTML($core->blog->name) => '',
+			'<span class="page-title">'.__('Categories').'</span>' => ''
+		))
+);
+
+if (!empty($_GET['del'])) {
+        dcPage::message(__('The category has been successfully removed.',
+			   'The categories have been successfully removed.',
+			   (int) $_GET['del']
+			   )
+			);
+}
+if (!empty($_GET['reord'])) {
+	dcPage::message(__('Categories have been successfully reordered.'));
+}
+
+$categories_combo = array();
+if (!$rs->isEmpty())
+{
+	while ($rs->fetch()) {
+		$catparents_combo[] = $categories_combo[] = new formSelectOption(
+			str_repeat('&nbsp;&nbsp;',$rs->level-1).($rs->level-1 == 0 ? '' : '&bull; ').html::escapeHTML($rs->cat_title),
+			$rs->cat_id
+		);
+	}
+}
+
+echo
+'<p class="top-add"><a class="button add" href="category.php">'.__('New category').'</a></p>';
+
+echo
+'<div class="col">';
+if ($rs->isEmpty())
+{
+	echo '<p>'.__('No category yet.').'</p>';
+}
+else
+{
+	echo
+	'<form action="categories.php" method="post" id="form-categories">'.
+	'<h3>'.__('List of blog\'s categories').'</h3>'.
+	'<div id="categories">';
+
+	$ref_level = $level = $rs->level-1;
+	while ($rs->fetch())
+	{
+		$attr = 'id="cat_'.$rs->cat_id.'"';
+		if ($rs->nb_total == 0) {
+			$attr .= ' class="deletable"';
+		}
+
+		if ($rs->level > $level) {
+			echo str_repeat('<ul><li '.$attr.'>',$rs->level - $level);
+		} elseif ($rs->level < $level) {
+			echo str_repeat('</li></ul>',-($rs->level - $level));
+		}
+
+		if ($rs->level <= $level) {
+			echo '</li><li '.$attr.'>';
+		}
+
+		echo
+		'<p>'.
+		form::checkbox(array('categories[]','cat-'.$rs->cat_id),$rs->cat_id).
+		'<label class="classic" for="cat-'.$rs->cat_id.'"><a href="category.php?id='.$rs->cat_id.'">'.html::escapeHTML($rs->cat_title).'</a></label>'.
+		' (<a href="posts.php?cat_id='.$rs->cat_id.'">'.
+		sprintf(($rs->nb_post > 1 ? __('%d entries') : __('%d entry') ),$rs->nb_post).'</a>'.
+		', '.__('total:').' '.$rs->nb_total.')</p>'.
+		'<p>'.__('URL:').' '.html::escapeHTML($rs->cat_url).'</p>';
+
+		$level = $rs->level;
+	}
+
+	if ($ref_level - $level < 0) {
+		echo str_repeat('</li></ul>',-($ref_level - $level));
+	}
+	echo
+	'</div>';
+
+	if (count($combo_action)>0) {
+		 echo
+		 '<div class="two-cols">'.
+		 '<p class="col checkboxes-helpers"></p>'.
+		 '<p class="col right"><label for="mov_cat">'.__('And choose the category which will receive its entries:').'</label> '.
+		 form::combo('mov_cat',array_merge(array(__('(No cat)') => ''),$categories_combo),'','maximal').'</p> '.
+		 '<p class="col right"><label for="action" class="classic">'.__('Selected categories action:').'</label> '.
+		 form::combo('action',$combo_action).
+		 $core->formNonce().
+		 '<input type="submit" value="'.__('ok').'" /></p>'.
+		 '</div>'.
+		 '</form>';
+	}
+
+	echo
+	'<div class="col clear">'.
+	'<h3>'.__('Categories order').'</h3>'.
+	'<form action="categories.php" method="post" id="reset-order">'.
+	'<!-- <p>'.__('This will relocate all categories on the top level').'</p> -->'.
+	'<p><input type="submit" value="'.__('Reorder all categories on the top level').'" />'.
+	form::hidden(array('reset'),1).
+	$core->formNonce().'</p>'.
+	'</form>'.
+	'</div>';
+
+	if (!$core->auth->user_prefs->accessibility->nodragdrop
+		&& $core->auth->check('categories',$core->blog->id)
+		&& $rs->count()>1) {
+		echo
+		'<div class="col">'.
+		'<form action="categories.php" method="post">'.
+		'<p>'.__('To rearrange categories order, move items by drag and drop, then click on “Save categories order” button.').'</p>'.
+		'<p>'.
+		'<input type="hidden" id="categories_order" name="categories_order" value=""/>'.
+		'<input type="submit" id="save-set-order" value="'.__('Save categories order').'" />'.
+		$core->formNonce().'</p>'.
+		'</form>'.
+		'</div>';
+	}
+}
+
+echo '</div>';
+
+dcPage::helpBlock('core_categories');
+dcPage::close();
+?>
---
+++ admin/media.php
@@ -0,0 +1,500 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+#
+# This file is part of Dotclear 2.
+#
+# Copyright (c) 2003-2013 Olivier Meunier & Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------
+
+/* HTML page
+-------------------------------------------------------- */
+require dirname(__FILE__).'/../inc/admin/prepend.php';
+
+dcPage::check('media,media_admin');
+
+$post_id = !empty($_GET['post_id']) ? (integer) $_GET['post_id'] : null;
+if ($post_id) {
+	$post = $core->blog->getPosts(array('post_id'=>$post_id,'post_type'=>''));
+	if ($post->isEmpty()) {
+		$post_id = null;
+	}
+	$post_title = $post->post_title;
+	$post_type = $post->post_type;
+	unset($post);
+}
+$d = isset($_REQUEST['d']) ? $_REQUEST['d'] : null;
+$dir = null;
+
+$page = !empty($_GET['page']) ? $_GET['page'] : 1;
+$nb_per_page =  30;
+
+# We are on home not comming from media manager
+if ($d === null && isset($_SESSION['media_manager_dir'])) {
+	# We get session information
+	$d = $_SESSION['media_manager_dir'];
+}
+
+if (!isset($_GET['page']) && isset($_SESSION['media_manager_page'])) {
+	$page = $_SESSION['media_manager_page'];
+}
+
+# We set session information about directory and page
+if ($d) {
+	$_SESSION['media_manager_dir'] = $d;
+} else {
+	unset($_SESSION['media_manager_dir']);
+}
+if ($page != 1) {
+	$_SESSION['media_manager_page'] = $page;
+} else {
+	unset($_SESSION['media_manager_page']);
+}
+
+# Sort combo
+$sort_combo = array(
+	__('By names, in ascending order') => 'name-asc',
+	__('By names, in descending order') => 'name-desc',
+	__('By dates, in ascending order') => 'date-asc',
+	__('By dates, in descending order') => 'date-desc'
+	);
+
+if (!empty($_GET['file_sort']) && in_array($_GET['file_sort'],$sort_combo)) {
+	$_SESSION['media_file_sort'] = $_GET['file_sort'];
+}
+$file_sort = !empty($_SESSION['media_file_sort']) ? $_SESSION['media_file_sort'] : null;
+
+$popup = (integer) !empty($_GET['popup']);
+
+$page_url = 'media.php?popup='.$popup.'&post_id='.$post_id;
+
+if ($popup) {
+	$open_f = array('dcPage','openPopup');
+	$close_f = array('dcPage','closePopup');
+} else {
+	$open_f = array('dcPage','open');
+	$close_f = create_function('',"dcPage::helpBlock('core_media'); dcPage::close();");
+}
+
+$core_media_writable = false;
+try {
+	$core->media = new dcMedia($core);
+	if ($file_sort) {
+		$core->media->setFileSort($file_sort);
+	}
+	$core->media->chdir($d);
+	$core->media->getDir();
+	$core_media_writable = $core->media->writable();
+	$dir =& $core->media->dir;
+	if  (!$core_media_writable) {
+		throw new Exception('you do not have sufficient permissions to write to this folder: ');
+	}
+} catch (Exception $e) {
+	$core->error->add($e->getMessage());
+}
+
+# Zip download
+if (!empty($_GET['zipdl']) && $core->auth->check('media_admin',$core->blog->id))
+{
+	try
+	{
+		@set_time_limit(300);
+		$fp = fopen('php://output','wb');
+		$zip = new fileZip($fp);
+		$zip->addExclusion('#(^|/).(.*?)_(m|s|sq|t).jpg$#');
+		$zip->addDirectory($core->media->root.'/'.$d,'',true);
+		
+		header('Content-Disposition: attachment;filename='.($d ? $d : 'media').'.zip');
+		header('Content-Type: application/x-zip');
+		$zip->write();
+		unset($zip);
+		exit;
+	}
+	catch (Exception $e)
+	{
+		$core->error->add($e->getMessage());
+	}
+}
+
+# New directory
+if ($dir && !empty($_POST['newdir']))
+{
+	try {
+		$core->media->makeDir($_POST['newdir']);
+		http::redirect($page_url.'&d='.rawurlencode($d).'&mkdok=1');
+	} catch (Exception $e) {
+		$core->error->add($e->getMessage());
+	}
+}
+
+# Adding a file
+if ($dir && !empty($_FILES['upfile'])) {
+  // only one file per request : @see option singleFileUploads in admin/js/jsUpload/jquery.fileupload
+	$upfile = array('name' => $_FILES['upfile']['name'][0],
+		'type' => $_FILES['upfile']['type'][0],
+		'tmp_name' => $_FILES['upfile']['tmp_name'][0],
+		'error' => $_FILES['upfile']['error'][0],
+		'size' => $_FILES['upfile']['size'][0]
+		);
+
+	if (!empty($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+		header('Content-type: application/json');
+		$message = array();
+
+		try {
+			files::uploadStatus($upfile);
+			$new_file_id = $core->media->uploadFile($upfile['tmp_name'], $upfile['name']);
+
+			$message['files'][] = array('name' => $upfile['name'],
+				'size' => $upfile['size'],
+				'html' => mediaItemLine($core->media->getFile($new_file_id), 1)
+				);
+		} catch (Exception $e) {
+			$message['files'][] = array('name' => $upfile['name'],
+				'size' => $upfile['size'],
+				'error' => $e->getMessage()
+				);
+		}
+		echo json_encode($message);
+		exit();
+	} else {
+		try {
+			files::uploadStatus($upfile);
+
+			$f_title = (isset($_POST['upfiletitle']) ? $_POST['upfiletitle'] : '');
+			$f_private = (isset($_POST['upfilepriv']) ? $_POST['upfilepriv'] : false);
+
+			$core->media->uploadFile($upfile['tmp_name'], $upfile['name'], $f_title, $f_private);
+			http::redirect($page_url.'&d='.rawurlencode($d).'&upok=1');
+		} catch (Exception $e) {
+			$core->error->add($e->getMessage());
+		}
+	}
+}
+
+# Removing item
+if ($dir && !empty($_POST['rmyes']) && !empty($_POST['remove']))
+{
+	$_POST['remove'] = rawurldecode($_POST['remove']);
+	
+	try {
+		$core->media->removeItem($_POST['remove']);
+		http::redirect($page_url.'&d='.rawurlencode($d).'&rmfok=1');
+	} catch (Exception $e) {
+		$core->error->add($e->getMessage());
+	}
+}
+
+# Rebuild directory
+if ($dir && $core->auth->isSuperAdmin() && !empty($_POST['rebuild']))
+{
+	try {
+		$core->media->rebuild($d);
+		http::redirect($page_url.'&d='.rawurlencode($d).'&rebuildok=1');
+	} catch (Exception $e) {
+		$core->error->add($e->getMessage());
+	}
+}
+
+# DISPLAY confirm page for rmdir & rmfile
+if ($dir && !empty($_GET['remove']) && empty($_GET['noconfirm']))
+{
+	call_user_func($open_f,__('Media manager'),'',
+		dcPage::breadcrumb(
+			array(
+				html::escapeHTML($core->blog->name) => '',
+				__('Media manager') => '',
+				'<span class="page-title">'.__('confirm removal').'</span>' => ''
+			),!$popup)
+	);
+	
+	echo
+	'<form action="'.html::escapeURL($page_url).'" method="post">'.
+	'<p>'.sprintf(__('Are you sure you want to remove %s?'),
+		html::escapeHTML($_GET['remove'])).'</p>'.
+	'<p><input type="submit" value="'.__('Cancel').'" /> '.
+	' &nbsp; <input type="submit" name="rmyes" value="'.__('Yes').'" />'.
+	form::hidden('d',$d).
+	$core->formNonce().
+	form::hidden('remove',html::escapeHTML($_GET['remove'])).'</p>'.
+	'</form>';
+	
+	call_user_func($close_f);
+	exit;
+}
+
+/* DISPLAY Main page
+-------------------------------------------------------- */
+$core->auth->user_prefs->addWorkspace('interface');
+$user_ui_enhanceduploader = $core->auth->user_prefs->interface->enhanceduploader;
+
+
+if (!isset($core->media)) {
+	$breadcrumb = dcPage::breadcrumb(
+		array(
+			html::escapeHTML($core->blog->name) => '',
+			'<span class="page-title">'.__('Media manager').'</span>' => ''
+		),!$popup);
+} else {
+	$breadcrumb_media = $core->media->breadCrumb(html::escapeURL($page_url).'&amp;d=%s','<span class="page-title">%s</span>');
+	if ($breadcrumb_media == '') {
+		$breadcrumb = dcPage::breadcrumb(
+			array(
+				html::escapeHTML($core->blog->name) => '',
+				'<span class="page-title">'.__('Media manager').'</span>' => ''
+			),!$popup);
+	} else {
+		$breadcrumb = dcPage::breadcrumb(
+			array(
+				html::escapeHTML($core->blog->name) => '',
+				__('Media manager') => html::escapeURL($page_url.'&d='),
+				$breadcrumb_media => ''
+			),!$popup);
+	}
+}
+
+call_user_func($open_f,__('Media manager'),
+	dcPage::jsLoad('js/_media.js').
+	($core_media_writable ? dcPage::jsUpload(array('d='.$d)) : ''),
+	$breadcrumb
+	);
+
+if (!empty($_GET['mkdok'])) {
+	dcPage::message(__('Directory has been successfully created.'));
+}
+
+if (!empty($_GET['upok'])) {
+	dcPage::message(__('Files have been successfully uploaded.'));
+}
+
+if (!empty($_GET['rmfok'])) {
+	dcPage::message(__('File has been successfully removed.'));
+}
+
+if (!empty($_GET['rmdok'])) {
+	dcPage::message(__('Directory has been successfully removed.'));
+}
+
+if (!empty($_GET['rebuildok'])) {
+	dcPage::message(__('Directory has been successfully rebuilt.'));
+}
+
+if (!empty($_GET['unzipok'])) {
+	dcPage::message(__('Zip file has been successfully extracted.'));
+}
+
+if (!$dir) {
+	call_user_func($close_f);
+	exit;
+}
+
+if ($post_id) {
+	echo '<p><strong>'.sprintf(__('Choose a file to attach to entry %s by clicking on %s.'),
+		'<a href="'.$core->getPostAdminURL($post_type,$post_id).'">'.html::escapeHTML($post_title).'</a>',
+		'<img src="images/plus.png" alt="'.__('Attach this file to entry').'" />').'</strong></p>';
+}
+if ($popup) {
+	echo '<p><strong>'.sprintf(__('Choose a file to insert into entry by clicking on %s.'),
+		'<img src="images/plus.png" alt="'.__('Attach this file to entry').'" />').'</strong></p>';
+}
+
+
+$items = array_values(array_merge($dir['dirs'],$dir['files']));
+echo '<div class="media-list">';
+if (count($items) == 0)
+{
+	echo '<p><strong>'.__('No file.').'</strong></p>';
+}
+else
+{
+	$pager = new pager($page,count($items),$nb_per_page,10);
+	$pager->html_prev = __($pager->html_prev);
+	$pager->html_next = __($pager->html_next);
+	
+	echo
+	'<form action="media.php" method="get">'.
+	'<p><label for="file_sort" class="classic">'.__('Sort files:').'</label> '.
+	form::combo('file_sort',$sort_combo,$file_sort).
+	form::hidden(array('popup'),$popup).
+	form::hidden(array('post_id'),$post_id).
+	'<input type="submit" value="'.__('Sort').'" /></p>'.
+	'</form>'.
+	
+	'<p class="pagination">'.__('Page(s)').' : '.$pager->getLinks().'</p>';
+	
+	for ($i=$pager->index_start, $j=0; $i<=$pager->index_end; $i++, $j++)
+	{
+		echo mediaItemLine($items[$i],$j);
+	}
+	
+	echo
+	'<p class="clear pagination">'.__('Page(s)').' : '.$pager->getLinks().'</p>';
+}
+if (!isset($pager)) {
+	echo
+	'<p class="clear"></p>';
+}
+echo
+'</div>';
+
+if ($core_media_writable)
+{
+	echo '<div class="two-cols">';
+	
+	if ($user_ui_enhanceduploader) {
+		echo
+		'<div class="col enhanced_uploader">';
+	} else {
+		echo
+		'<div class="col">';
+	}
+
+	echo
+	'<div class="fieldset">'.
+	'<h3>'.__('Add files').'</h3>'.
+	'<p>'.__('Please take care to publish media that you own and that are not protected by copyright.').'</p>'.
+	'<p class="max-sizer form-note info">&nbsp;'.__('Maximum file size allowed:').' '.files::size(DC_MAX_UPLOAD_SIZE).'</p>'.
+	'<form id="fileupload" action="'.html::escapeURL($page_url).'" method="post" enctype="multipart/form-data" aria-disabled="false">'.
+	'<p>'.form::hidden(array('MAX_FILE_SIZE'),DC_MAX_UPLOAD_SIZE).
+	$core->formNonce().'</p>'.
+	'<div class="fileupload-ctrl"><p class="queue-message"></p><ul class="files"></ul></div>';
+
+	echo
+	'<div class="fileupload-buttonbar clear">';
+
+	echo
+	'<p><label for="upfile">'.'<span class="add-label one-file">'.__('Choose file').'</span>'.'</label>'.
+	'<button class="button add">'.__('Choose files').'</button>'.
+	'<input type="file" id="upfile" name="upfile[]"'.($user_ui_enhanceduploader?' multiple="mutiple"':'').' data-url="'.html::escapeURL($page_url).'" /></p>';
+
+	echo
+	'<p class="one-file"><label for="upfiletitle">'.__('Title:').'</label>'.form::field(array('upfiletitle','upfiletitle'),35,255).'</p>'.
+	'<p class="one-file"><label for="upfilepriv" class="classic">'.__('Private').'</label> '.
+	form::checkbox(array('upfilepriv','upfilepriv'),1).'</p>';
+
+	if (!$user_ui_enhanceduploader) {
+		echo
+		'<p class="one-file form-help info">'.__('To send several files at the same time, you can activate the enhanced uploader in').
+		' <a href="preferences.php?tab=user-options">'.__('My preferences').'</a></p>';
+	}
+
+	echo
+	'<p class="clear"><button class="button clean">'.__('Refresh').'</button>'.
+	'<input class="button cancel one-file" type="reset" value="'.__('Clear all').'"/>'.
+	'<input class="button start" type="submit" value="'.__('Upload').'"/></p>'.
+	'</div>';
+
+	echo
+	'<p style="clear:both;">'.form::hidden(array('d'),$d).'</p>'.
+	'</form>'.
+	'</div></div>';
+
+	echo
+	'<div class="col">'.
+	'<form class="clear fieldset" action="'.html::escapeURL($page_url).'" method="post">'.
+	'<div id="new-dir-f">'.
+	'<h3>'.__('New directory').'</h3>'.
+	$core->formNonce().
+	'<p><label for="newdir">'.__('Directory Name:').'</label>'.
+	form::field(array('newdir','newdir'),35,255).'</p>'.
+	'<p><input type="submit" value="'.__('Create').'" />'.
+	form::hidden(array('d'),html::escapeHTML($d)).'</p>'.
+	'</div>'.
+	'</form></div>';
+	
+	echo '</div>';
+}
+
+# Empty remove form (for javascript actions)
+echo
+'<form id="media-remove-hide" action="'.html::escapeURL($page_url).'" method="post"><div class="clear">'.
+form::hidden('rmyes',1).form::hidden('d',html::escapeHTML($d)).
+form::hidden('remove','').
+$core->formNonce().
+'</div></form>';
+
+# Get zip directory
+if ($core->auth->check('media_admin',$core->blog->id) && 
+	!(count($items) == 0 || (count($items) == 1 && $items[0]->parent)))
+{
+	echo
+	'<p class="zip-dl"><a href="'.html::escapeURL($page_url).'&amp;zipdl=1">'.
+	__('Download this directory as a zip file').'</a></p>';
+}
+
+call_user_func($close_f);
+
+/* ----------------------------------------------------- */
+function mediaItemLine($f,$i)
+{
+	global $core, $page_url, $popup, $post_id;
+	
+	$fname = $f->basename;
+	
+	if ($f->d) {
+		$link = html::escapeURL($page_url).'&amp;d='.html::sanitizeURL($f->relname);
+		if ($f->parent) {
+			$fname = '..';
+		}
+	} else {
+		$link =
+		'media_item.php?id='.$f->media_id.'&amp;popup='.$popup.'&amp;post_id='.$post_id;
+	}
+	
+	$class = 'media-item media-col-'.($i%2);
+	
+	$res =
+	'<div class="'.$class.'"><a class="media-icon media-link" href="'.$link.'">'.
+	'<img src="'.$f->media_icon.'" alt="" /></a>'.
+	'<ul>'.
+	'<li><a class="media-link" href="'.$link.'">'.$fname.'</a></li>';
+	
+	if (!$f->d) {
+		$res .=
+		'<li>'.$f->media_title.'</li>'.
+		'<li>'.
+		$f->media_dtstr.' - '.
+		files::size($f->size).' - '.
+		'<a href="'.$f->file_url.'">'.__('open').'</a>'.
+		'</li>';
+	}
+	
+	$res .= '<li class="media-action">&nbsp;';
+	
+	if ($post_id && !$f->d) {
+		$res .= '<form action="post_media.php" method="post">'.
+		'<input type="image" src="images/plus.png" alt="'.__('Attach this file to entry').'" '.
+		'title="'.__('Attach this file to entry').'" /> '.
+		form::hidden('media_id',$f->media_id).
+		form::hidden('post_id',$post_id).
+		form::hidden('attach',1).
+		$core->formNonce().
+		'</form>';
+	}
+	
+	if ($popup && !$f->d) {
+		$res .= '<a href="'.$link.'"><img src="images/plus.png" alt="'.__('Insert this file into entry').'" '.
+		'title="'.__('Insert this file into entry').'" /></a> ';
+	}
+	
+	if ($f->del) {
+		$res .= '<a class="media-remove" '.
+		'href="'.html::escapeURL($page_url).'&amp;d='.
+		rawurlencode($GLOBALS['d']).'&amp;remove='.rawurlencode($f->basename).'">'.
+		'<img src="images/trash.png" alt="'.__('Delete').'" title="'.__('delete').'" /></a>';
+	}
+	
+	$res .= '</li>';
+	
+	if ($f->type == 'audio/mpeg3') {
+		$res .= '<li>'.dcMedia::mp3player($f->file_url,'index.php?pf=player_mp3.swf').'</li>';
+	}
+	
+	$res .= '</ul></div>';
+	
+	return $res;
+}
+?>
