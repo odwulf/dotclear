6cca3764dc24bf550b488dbaccc256c6f24a5870
Nicolas <nikrou77@gmail.com>
2013-08-24 12:22:49 +0200

    Revert legend_click : it does not work as expected

---
+++ admin/js/_categories.js
@@ -0,0 +1,35 @@
+$(function() {
+	if ($.fn['nestedSortable']!==undefined) {
+		$('#categories ul li').css('cursor','move');
+		$('#save-set-order').prop('disabled',true).addClass('disabled');
+		$('#categories ul').nestedSortable({
+			listType: 'ul',
+			items: 'li',
+			placeholder: 'placeholder',
+			update: function() {
+				$('#categories_order').attr('value',JSON.stringify($('#categories ul').nestedSortable('toArray')));
+				$('#save-set-order').prop('disabled',false).removeClass('disabled');
+			}
+		});
+	}
+
+	$('.checkboxes-helpers').each(function() {
+		dotclear.checkboxesHelpers(this);
+	});
+
+	$('#mov_cat').parent().hide();
+	$('input[name="categories[]"]').click(function() {
+		if ($('input[name="categories[]"].notempty:checked').length>0) {
+			$('#mov_cat').parent().show().parent().removeClass('two-cols').addClass('three-cols');
+		} else {
+			$('#mov_cat').parent().hide().parent().removeClass('three-cols').addClass('two-cols');
+		}
+	});
+
+	dotclear.categoriesActionsHelper();
+
+
+	$('form#reset-order').submit(function() {
+		return window.confirm(dotclear.msg.confirm_reorder_categories);
+	});
+});
