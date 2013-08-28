9c9e4e17a2b9dec5a52b9cc0c8843413a0758afe
Lepeltier k?vin <Lepeltier k?vin@localhost>
2013-08-28 18:45:31 +0200

    Ticket #1604

---
+++ admin/js/prelude.js
@@ -0,0 +1,26 @@
+// Accessibility links thks to vie-publique.fr
+aFocus = function() {
+	if(document.getElementById("prelude")) {
+		var aElts = document.getElementById("prelude").getElementsByTagName("A");
+		for (var i=0; i<aElts.length; i++) {
+			aElts[i].className="hidden";
+			aElts[i].onfocus=function() {
+				$('#prelude a').removeClass('hidden');
+				$('#wrapper').css('padding-top', '1em');
+			}
+		}
+	}
+}
+// events onload
+function addLoadEvent(func) {
+	if (window.addEventListener)
+		window.addEventListener("load", func, false);
+	else if (window.attachEvent)
+		window.attachEvent("onload", func);
+}
+addLoadEvent(aFocus);
+
+// init
+$(function() {
+	$('#wrapper').css('padding-top', '0');
+});
